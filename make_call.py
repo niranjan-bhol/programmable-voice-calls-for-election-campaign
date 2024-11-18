513c0ef87d89a733677bbb89a65b18ab4e685809
Niranjan Bhol <niranjan.bhol@icloud.com>
2024-11-18 09:54:20 +0530

    update resource google drive, update TwiML & url

--- make_call.py
+++ make_call.py
@@ -1,22 +1,19 @@
 import os
 from twilio.rest import Client
 
-# Twilio credentials
 account_sid = "AC63780e42e4f538b86e6281d00f6ec38e"
 auth_token = "3b14fd99dd0a12f57c7cb9da1d5b7cc7"
 client = Client(account_sid, auth_token)
 
-# Read numbers from numbers.txt
 with open("numbers.txt", "r") as file:
     numbers = file.read().splitlines()
 
-# Iterate through numbers and make calls
 for number in numbers:
     try:
         call = client.calls.create(
             from_="+13479605682",
-            to=number.strip(),  # Remove any extra spaces
-            url="https://handler.twilio.com/twiml/EH7a4345c1f4b2402da4f3de813d4b9037"  # TwiML URL for the call
+            to=number.strip(),
+            url="https://handler.twilio.com/twiml/EH7158f3e4b2766aa9af39d4b811e2d67e"
         )
         print(f"Call initiated to {number}: SID {call.sid}")
     except Exception as e:
